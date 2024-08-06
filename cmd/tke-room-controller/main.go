a580208cc164d8af7ee4942b88d1ea8098c9f224
roc <roc@imroc.cc>
2024-08-06 18:04:45 +0800

    update at 2024-08-06 18:04:45

--- cmd/tke-room-controller/main.go
+++ cmd/tke-room-controller/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/imroc/tke-room-manager/cmd/app"
+import "github.com/imroc/tke-room-manager/cmd/tke-room-controller/app"
 
 func main() {
 	if err := app.RootCommand.Execute(); err != nil {
